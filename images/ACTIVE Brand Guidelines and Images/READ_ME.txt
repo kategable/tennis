02fe2ca5d4a7cab35baef3d827f18e456fcea337
Kate Sky <kateskytech@gmail.com>
2017-12-02 22:24:34 -0600

    latest changes

---
+++ images/ACTIVE Brand Guidelines and Images/READ_ME.txt
@@ -0,0 +1,23 @@
+Powered By Logos:
+
+This logo should be placed in the area that contains a list of any ACTIVE Network or Reserve America (Campground API) activities and events. Preferred placement is at the top right of the area (if space permits). Please use the logo color that fits best with your website/application. Do not modify these logos in any way.
+
+
+Register Now Buttons:
+
+This button should be used to link directly to the event details page for each event. Please use the button color that fits best with your website/application. Do not modify these buttons in any way.
+
+
+ACTIVE Icons:
+
+This icon should can be placed next to any ACTIVE Network activities and events listed on a calendar, list, or other format. Preferred placement is directly next to the event title or link if space permits). Please use the logo color that fits best with your website/application. Do not modify these logos in any way.
+
+
+Reserve Now Buttons (Campground API):
+
+This button should be used to link directly to the campground details page for each facility. Please use the button color that fits best with your website/application. Do not modify these buttons in any way.
+
+
+ReserveAmerica Icons (Campground API):
+
+This icon should can be placed next to any ReserveAmerica facility listed on a map, list, or other format. Preferred placement is directly next to the facility title or link if space permits). Please use the logo color that fits best with your website/application. Do not modify these logos in any way.
--- js/tennis.js
+++ js/tennis.js
@@ -1,25 +1,46 @@
 function tennisApi() {
 
-    var api = {};
-    var apikey= "pw9km4p5mx6nx8jdxzd45h73";
-    function load(pos) {
-    var url ="https://api.amp.active.com/v2/search/?lat_lon=42.34%2C-88.04&radius=50&current_page=1&per_page=10&sort=distance&topic=tennis&exclude_children=false&api_key=pw9km4p5mx6nx8jdxzd45h73";
-//            http://api.amp.active.com/v2/search
-      
-       $.getJSON( url, function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });
-       
-        $( "<ul/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
-      });
-    }
-    api.load = load;
+  var api = {};
+  var apikey = "pw9km4p5mx6nx8jdxzd45h73";
+  function load(pos) {
+    var url = "https://api.amp.active.com/v2/search/?lat_lon=42.34%2C-88.04&radius=50&current_page=1&per_page=10&sort=distance&topic=tennis&exclude_children=false&api_key=pw9km4p5mx6nx8jdxzd45h73";
+    //            http://api.amp.active.com/v2/search
+    // $.ajax({
+    //   url: url,
+    //   type: 'GET',
+    //   dataType: 'json',
+    //   success: function () {
+    //     var items = [];
+    //     $.each(data, function (key, val) {
+    //       items.push("<li id='" + key + "'>" + val + "</li>");
+    //     });
 
-    return api;
+    //     $("<ul/>", {
+    //       "class": "my-new-list",
+    //       html: items.join("")
+    //     }).appendTo("body");
+    //   },
+    //   error: function () { alert('boo!'); },
+    //   beforeSend: setHeader
+    // });
+
+    // function setHeader(xhr) {
+    //   xhr.setRequestHeader('X-Originating-Ip', '98.220.98.123');
+    // };
+    $.getJSON(url, function (data) {
+      var items = [];
+      $.each(data, function (key, val) {
+        items.push("<li id='" + key + "'>" + val + "</li>");
+      });
+
+      $("<ul/>", {
+        "class": "my-new-list",
+        html: items.join("")
+      }).appendTo("body");
+    });
+  }
+  api.load = load;
+
+  return api;
 
 };
